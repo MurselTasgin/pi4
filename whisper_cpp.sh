7dd9925d5d08a86442488684c61fb26027d791ae
MurselTasgin <MurselTasgin@users.noreply.github.com>
2024-02-07 20:24:30 +0100

    Update whisper_cpp.sh

--- whisper_cpp.sh
+++ whisper_cpp.sh
@@ -1,4 +1,4 @@
 eval "$(conda shell.bash hook)"
 conda activate kenv1
 cd /home/kerem/projects/whisper.cpp 
-./stream -m models/ggml-base.bin --step 4000 --length 8000 -c 0 -t 4 -ac 512 -l tr  
+./stream -m models/ggml-base.bin --step 4000 --length 8000 -c 0 -t 4 -ac 512 -l tr  --topic_name topic_100
